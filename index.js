b0f65370648e67d9745474ce8a2e72b8f2f604d3
elioghanem <137090109+elioghanem@users.noreply.github.com>
2023-07-28 06:37:53 +0300

    making a burger menu for the responsive

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function toggleAnswer(question) {
         answer.style.display = "block";
     }
 }
+
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
